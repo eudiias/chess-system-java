3dd8a4fdda1a51afed0e2f4accf6a99f70e9d100
Carlos Dias <carlos.dias.o.n@gmail.com>
2026-07-19 05:13:12 -0300

    Feat: Moving pieces

--- src/Application/Main.java
+++ src/Application/Main.java
@@ -1,7 +1,24 @@
 import Application.UI;
 import Chess.ChessMatch;
+import Chess.ChessPiece;
+import Chess.ChessPosition;
 
 void main() {
+
+    Scanner sc = new Scanner(System.in);
     ChessMatch chessMatch = new ChessMatch();
-    UI.printBoard(chessMatch.getPieces());
+
+    while (true)
+    {
+        UI.printBoard(chessMatch.getPieces());
+        System.out.println();
+
+        System.out.print("Print source: ");
+        ChessPosition source = UI.readChessPosition(sc);
+
+        System.out.print("Print target: ");
+        ChessPosition target = UI.readChessPosition(sc);
+
+        ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
+    }
 }
