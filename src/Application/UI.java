a6c185e811b7c7981ac9b804bab38d3d104f8269
Carlos Dias <carlos.dias.o.n@gmail.com>
2026-07-21 01:49:09 -0300

    Feat: Printing possible moves

--- src/Application/UI.java
+++ src/Application/UI.java
@@ -15,6 +15,7 @@ public class UI {
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_YELLOW = "\u001B[33m";
     public static final String ANSI_WHITE = "\u001B[37m";
+    public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
 
     public static void clearScreen() {
         System.out.print("\033[H\033[2J");
@@ -43,18 +44,37 @@ public class UI {
             System.out.print((8 - i) + " ");
             for (int j = 0; j < pieces.length; j++)
             {
-                printPiece(pieces[i][j]);
+                printPiece(pieces[i][j], false);
             }
             System.out.println();
         }
         System.out.println("  a  b  c  d  e  f  g  h");
     }
 
-    private static void printPiece(ChessPiece piece)
+    public static void printBoard(ChessPiece[][] pieces, boolean[][] possiblesMoves)
     {
+
+        for (int i = 0; i < pieces.length; i++)
+        {
+            System.out.print((8 - i) + " ");
+            for (int j = 0; j < pieces.length; j++)
+            {
+                printPiece(pieces[i][j], possiblesMoves[i][j]);
+            }
+            System.out.println();
+        }
+        System.out.println("  a  b  c  d  e  f  g  h");
+    }
+
+    private static void printPiece(ChessPiece piece, boolean background)
+    {
+        if (background)
+        {
+            System.out.print(ANSI_BLUE_BACKGROUND);
+        }
         if (piece == null)
         {
-            System.out.print("-");
+            System.out.print("-" + ANSI_RESET);
         }
         else
         {
