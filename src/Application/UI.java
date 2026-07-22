1bad7e4134c5971ebebd0a84d6810e00f1510bf3
Carlos Dias <carlos.dias.o.n@gmail.com>
2026-07-22 03:00:32 -0300

    Feat: Check logic

--- src/Application/UI.java
+++ src/Application/UI.java
@@ -46,6 +46,11 @@ public class UI {
         System.out.println();
         System.out.println("Turn: " + chessMatch.getTurn());
         System.out.println("Waiting player: " + chessMatch.getCurrentPlayer());
+
+        if (chessMatch.getCheck())
+        {
+            System.out.println("CHECK!");
+        }
     }
 
     public static void printBoard(ChessPiece[][] pieces)
@@ -111,9 +116,8 @@ public class UI {
         System.out.print(ANSI_WHITE);
         white.forEach(System.out::print);
         System.out.println();
-        System.out.print("Black: [");
+        System.out.print("Black: ");
         System.out.print(ANSI_YELLOW);
         black.forEach(System.out::print);
-        System.out.print(" ]");
     }
 }
